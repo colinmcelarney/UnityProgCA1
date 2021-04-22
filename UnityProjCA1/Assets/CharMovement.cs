9f82d8b219b39acff1234cb1576819fbc71ac177
Colin McElarney <71757780+colinmcelarney@users.noreply.github.com>
2021-04-22 16:52:39 +0100

    Update 22/4/2021
    Added camera to player and  jumping power

---
+++ UnityProjCA1/Assets/CharMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script defines how the player moves throughout the game. It defines the speed and power of the players jump
+public class CharMovement : MonoBehaviour
+{
+    public float speed = 30; //speed of player when moving
+    public float jumpPower = 8; //player can jump power of 4
+
+    Rigidbody rb; //player is a rigid body
+    CapsuleCollider col; //player is a collider
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal") * speed;
+        float Vertical = Input.GetAxis("Vertical") * speed;
+            Horizontal *= Time.deltaTime;
+            Vertical *= Time.deltaTime;
+            transform.Translate(Horizontal, 0, Vertical);
+
+            if (isGrounded() && Input.GetKeyDown (KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+            if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None; //if user presses the escape button the mouse position becomes back in focus
+    }
+
+        private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
+    }
+}
---
+++ UnityProjCA1/Assets/Mouse.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+//this script allows mouse movement from the players perspective
+public class Mouse : MonoBehaviour
+{
+    private GameObject player;
+    private float minClamp = -45;
+    private float maxClamp = 45;
+    public Vector2 rotation;
+    private Vector2 currentLookRot;
+    private Vector2 rotationV = new Vector2(0, 0);
+    public float lookSensitivity = 2;
+    public float lookSmoothDamp = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse Y") * lookSensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, minClamp, maxClamp);
+        player.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * lookSensitivity);
+        currentLookRot.y = Mathf.SmoothDamp(currentLookRot.y, rotation.y, ref rotationV.y, lookSmoothDamp);
+        transform.localEulerAngles = new Vector3(-currentLookRot.y, 0, 0);
+    }
+}
